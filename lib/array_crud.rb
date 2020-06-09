1675e959da48c4e85c778ddf60f691c200c56543
Sean B <butts.sean01@gmail.com>
2020-06-09 01:14:32 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -21,7 +21,7 @@ def remove_element_from_end_of_array(array)
 end
 
 def remove_element_from_start_of_array(array)
-  create_an_array.
+  create_an_array.shift("wow")
 end
 
 def retrieve_element_from_index(array, index_number)
@@ -29,7 +29,7 @@ def retrieve_element_from_index(array, index_number)
 end
 
 def retrieve_first_element_from_array(array)
-  create_an_array.first 
+  create_an_array[0]
 end
 
 def retrieve_last_element_from_array(array)
