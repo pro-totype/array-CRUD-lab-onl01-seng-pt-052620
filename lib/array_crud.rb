36f6f5d0125af6bd2650c4cf8ab2e8b47d3c766a
Sean B <butts.sean01@gmail.com>
2020-06-09 01:02:35 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -9,7 +9,7 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  create_an_array.push("bulldog")
+  create_an_array("bulldog")
 end
 
 def add_element_to_start_of_array(array, element)
