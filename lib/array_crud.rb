aab48b24157cb235632decb191bd157e8d42f186
Sean B <butts.sean01@gmail.com>
2020-06-09 01:21:20 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -14,11 +14,12 @@ def add_element_to_end_of_array(array, element)
 end
 
 def add_element_to_start_of_array(array, element)
-  add_element_to_start_of_array("wow")
+  add_element_to_start_of_array=["I", "am", "really", "learning"]
+  add_element_to_start_of_array.unshift("wow")
 end
 
 def remove_element_from_end_of_array(array)
-  create_an_array.pop("arrays!")
+  remove_element_from_end_of_array=
 end
 
 def remove_element_from_start_of_array(array)
